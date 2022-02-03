1d19024ac34e02ff4a8bcf87cdc3e990c73ac5dd
Antoni <antoniibrahimdev@gmail.com>
2022-02-03 16:44:34 -0500

    fixed height issues, added javascript for portfolio tab and cursor change

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const portfolio = {};
 
 portfolio.init = () => {
-
+    portfolio.switchPortfolioFromFeatured();
 }
 
 const portfolioBars = document.querySelector(".pBarP");
+const featured = document.querySelector(".featured");
+const all = document.querySelector(".projectDisplay");
 
-portfolio.switchPortfolioTab = () => {
+portfolio.switchPortfolioFromFeatured = () => {
     portfolioBars.addEventListener("click", () => {
-        
+        featured.style.display = "none";
+        all.style.display = "block";
     })
 }
 portfolio.init();
